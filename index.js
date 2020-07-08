cbbe89db4f2350f5795930a442d4f1e4bef654ad
Spencer Carli <spencercarli@gmail.com>
2020-07-08 10:24:38 -0500

    Fix typo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
-import App from './App.js';
+// eslint-disable-next-line import/extensions
+import App from './App';
 
 AppRegistry.registerComponent(appName, () => App);
